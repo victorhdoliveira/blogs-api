2bfbfff3b59779ae665b83cbcd973ffb3686bfd4
Victor Hugo <victorhdoliveira@gmail.com>
2023-03-08 18:54:41 -0300

    feat: category model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,19 +2,19 @@ const { userService } = require('../services');
 const { createToken } = require('../auth/authFunctions');
 
 const insertUser = async (req, res) => {
-    try {
-        const { displayName, email, password, image } = req.body;
+  try {
+      const { displayName, email, password, image } = req.body;
 
-        await userService.createUser({ displayName, email, password, image });
-        const token = createToken(email);
-        
-        res.status(201).json({ token });
-    } catch (err) {
-        res.status(500).json({
-          message: 'Error saving user in database',
-          error: err.message,
-        });
-      }
+      await userService.createUser({ displayName, email, password, image });
+      const token = createToken(email);
+      
+      res.status(201).json({ token });
+  } catch (err) {
+      res.status(500).json({
+        message: 'Error saving user in database',
+        error: err.message,
+      });
+    }
 };
 
 const getUsers = async (req, res) => {
@@ -31,12 +31,14 @@ const getUsers = async (req, res) => {
 };
 
 const getUserId = async (req, res) => {
-  const { id } = req.params;
-  const user = await userService.getByUserId(id);
-  if (!user) {
-    return res.status(404).json({ message: 'User does not exist' });
-  }
-  return res.status(200).json(user);
+  try {
+    const { id } = req.params;
+    const user = await userService.getByUserId(id);
+    if (!user) throw Error;
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(404).json({ message: 'User does not exist' });
+}
 };
 
 module.exports = { 
---
+++ src/models/Category.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * @param {import('sequelize').Sequelize} sequelize
+ * @param {*} DataTypes
+ * @returns
+ */
+
+ module.exports = (sequelize, DataTypes) => {
+    const CategoryModel = sequelize.define('Category', {
+      id: { type: DataTypes.INTEGER, primaryKey: true },
+      name: DataTypes.STRING,
+    },
+    {
+      tableName: 'categories',
+      underscored: true,
+      timestamps: false,
+    },
+    );
+    return CategoryModel;
+  }
